25a55f5e1466724b0d29be2f5aea96ecf900e149
UsernameForGitHub21 <113929391+UsernameForGitHub21@users.noreply.github.com>
2023-02-10 21:23:18 -0500

    Assignment2
    Assignment 2

--- Col_Morgan_Movies/src/app/content-card/content-card.component.ts
+++ Col_Morgan_Movies/src/app/content-card/content-card.component.ts
@@ -1,60 +1,67 @@
 import { Component } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
+//import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
+
+
+
 export class ContentCardComponent {
-  myContentList : ContentList = new ContentList();
 
-  contentItem1 : Content = {
-    id: 0,
-    title: `title1`,
-    description: `description for title1`,
-    creator: `creator1`
-  }
-
-  contentItem2 : Content = {
-    id: 1,
-    title: `title2`,
-    description: `description for title2`,
-    creator: `creator2`
-  }
-
-  contentItem3 : Content = {
-    id: 2,
-    title: `title3`,
-    description: `description for title3`,
-    creator: `creator3`
-  }
-
-  contentItem4 : Content = {
-    id: 3,
-    title: `title4`,
-    description: `description for title4`,
-    creator: `creator4`
-  }
-
-  constructor(){
-    this.myContentList.add(this.contentItem1);
-    this.myContentList.add(this.contentItem2);
-    this.myContentList.add(this.contentItem3);
-    this.myContentList.add(this.contentItem4);
-
-  }
-
-  displayMyIndex(index : number) {
-    let myItems = this.myContentList.getItems();
-
-    if (myItems.length > 0 && index > 0 && index < myItems.length){
-      return this.myContentList.printIndex(index);
-    } else {
-      alert("ERROR");
-      return "";
-    }
-    
-  }
 }
+
+// export class ContentCardComponent {
+//   myContentList : ContentList = new ContentList();
+
+//   contentItem1 : Content = {
+//     id: 0,
+//     title: `title1`,
+//     description: `description for title1`,
+//     creator: `creator1`
+//   }
+
+//   contentItem2 : Content = {
+//     id: 1,
+//     title: `title2`,
+//     description: `description for title2`,
+//     creator: `creator2`
+//   }
+
+//   contentItem3 : Content = {
+//     id: 2,
+//     title: `title3`,
+//     description: `description for title3`,
+//     creator: `creator3`
+//   }
+
+//   contentItem4 : Content = {
+//     id: 3,
+//     title: `title4`,
+//     description: `description for title4`,
+//     creator: `creator4`
+//   }
+
+//   constructor(){
+//     this.myContentList.add(this.contentItem1);
+//     this.myContentList.add(this.contentItem2);
+//     this.myContentList.add(this.contentItem3);
+//     this.myContentList.add(this.contentItem4);
+
+//   }
+
+//   displayMyIndex(index : number) {
+//     let myItems = this.myContentList.getItems();
+
+//     if (myItems.length > 0 && index > 0 && index < myItems.length){
+//       return this.myContentList.printIndex(index);
+//     } else {
+//       alert("ERROR");
+//       return "";
+//     }
+    
+//   }
+// }
---
+++ Col_Morgan_Movies/src/app/content-list/content-list.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { ContentList } from '../helper-files/content-list';
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss']
+})
+export class ContentListComponent {
+
+    myContentList : ContentList = new ContentList();
+
+  contentItem1 : Content = {
+    id: 0,
+    title: `title1`,
+    description: `description for title1`,
+    creator: `creator1`
+  }
+
+  contentItem2 : Content = {
+    id: 1,
+    title: `title2`,
+    description: `description for title2`,
+    creator: `creator2`
+  }
+
+  contentItem3 : Content = {
+    id: 2,
+    title: `title3`,
+    description: `description for title3`,
+    creator: `creator3`
+  }
+
+  contentItem4 : Content = {
+    id: 3,
+    title: `title4`,
+    description: `description for title4`,
+    creator: `creator4`
+  }
+
+  contentItem5 : Content = {
+    id: 4,
+    title: `title5`,
+    description: `description for title5`,
+    creator: `creator5`
+  }
+  contentItem6 : Content = {
+    id: 5,
+    title: `title6`,
+    description: `description for title6`,
+    creator: `creator6`
+  }
+
+  constructor(){
+    this.myContentList.add(this.contentItem1);
+    this.myContentList.add(this.contentItem2);
+    this.myContentList.add(this.contentItem3);
+    this.myContentList.add(this.contentItem4);
+
+  }
+
+  displayMyIndex(index : number) {
+    let myItems = this.myContentList.getItems();
+
+    if (myItems.length > 0 && index > 0 && index < myItems.length){
+      return this.myContentList.printIndex(index);
+    } else {
+      alert("ERROR");
+      return "";
+    }
+    
+  }
+
+  logToConsole(item: { id: string; title: string; }){
+    console.log("ID: " + item.id + ", Title: " + item.title);
+  }
+}
